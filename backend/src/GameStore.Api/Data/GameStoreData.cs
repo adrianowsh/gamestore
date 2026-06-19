4ce860bd77abd96bfe9874b0db7301842259dfe2
Adriano Mota <adrianowsh@gmail.com>
2026-06-19 00:31:19 -0300

    feat: add game store dara in order to separate data concerns

---
+++ backend/src/GameStore.Api/Data/GameStoreData.cs
@@ -0,0 +1,77 @@
+﻿using GameStore.Api.Models.Games;
+
+namespace GameStore.Api.Data;
+
+public sealed class GameStoreData
+{
+    private readonly List<Genre> _genres;
+    private readonly List<Game> _games;
+
+    public GameStoreData()
+    {
+        _genres =
+            [
+                Genre.Create("Action-adventure"),
+                Genre.Create("Fighting"),
+                Genre.Create("Kids and Family"),
+                Genre.Create("Platform"),
+                Genre.Create("Role-playing"),
+                Genre.Create("Sports"),
+            ];
+
+        _games =
+        [
+            Game.Create("The Legend of Zelda: Breath of the Wild", "An open-world adventure game",
+                _genres.FirstOrDefault(g => g.Name == "Action-adventure")?.Id ?? string.Empty, 99.99m,
+                new DateOnly(2017, 3, 3)),
+            Game.Create("Super Mario Odyssey", "A 3D platformer game",
+                _genres.FirstOrDefault(g => g.Name == "Platform")?.Id ?? string.Empty, 120.99m,
+                new DateOnly(2017, 10, 27)),
+            Game.Create("Red Dead Redemption 2", "An epic Western action-adventure game",
+                _genres.FirstOrDefault(g => g.Name == "Action-adventure")?.Id ?? string.Empty, 150.99m,
+                new DateOnly(2018, 10, 26)),
+            Game.Create("Grand Theft Auto V", "An open-world action-adventure game",
+                _genres.FirstOrDefault(g => g.Name == "Action-adventure")?.Id ?? string.Empty, 59.99m,
+                new DateOnly(2013, 9, 17))
+        ];
+    }
+
+
+    /// <summary>
+    /// In a real application, these methods would interact with a database or other data storage mechanism to retrieve and manipulate game and genre data. For simplicity, this example uses in-memory lists to store the data.
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<Game> GetGames() => _games;
+
+    /// <summary>
+    /// In a real application, these methods would interact with a database or other data storage mechanism to retrieve and manipulate game and genre data. For simplicity, this example uses in-memory lists to store the data.
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<Genre> GetGenres() => _genres;
+
+    /// <summary>
+    /// In a real application, these methods would interact with a database or other data storage mechanism to retrieve and manipulate game and genre data. For simplicity, this example uses in-memory lists to store the data.
+    /// </summary>
+    /// <param name="gameId"></param>
+    /// <returns></returns>
+    public Game? GetGame(string gameId) => _games.FirstOrDefault(g => g.Id == gameId);
+
+    /// <summary>
+    /// In a real application, these methods would interact with a database or other data storage mechanism to retrieve and manipulate game and genre data. For simplicity, this example uses in-memory lists to store the data.
+    /// </summary>
+    /// <param name="game"></param>
+    public void AddGame(Game game) => _games.Add(game);
+
+    /// <summary>
+    /// In a real application, these methods would interact with a database or other data storage mechanism to retrieve and manipulate game and genre data. For simplicity, this example uses in-memory lists to store the data.
+    /// </summary>
+    /// <param name="game"></param>
+    public void RemoveGame(Game game)
+    {
+        Game? existingGame = GetGame(game.Id);
+        if (existingGame is not null)
+        {
+            _games.Remove(existingGame);
+        }
+    }
+}
--- backend/src/GameStore.Api/Program.cs
+++ backend/src/GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Data;
 using GameStore.Api.Models.Games;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -7,34 +8,21 @@ WebApplication app = builder.Build();
 
 const string GetGameEndpointName = "GetGame";
 
-List<Genre> genres =
-[
-  Genre.Create("Action-adventure"),
-  Genre.Create("Fighting"),
-  Genre.Create("Kids and Family"),
-  Genre.Create("Rolesplaying"),
-  Genre.Create("Sports"),
-];
-
-List<Game> games =
-[
-   Game.Create("The Legend of Zelda: Breath of the Wild", "An open-world adventure game", genres.FirstOrDefault(g => g.Name == "Action-adventure")?.Id ?? string.Empty, 99.99m, new DateOnly(2017, 3, 3)),
-   Game.Create("Super Mario Odyssey", "A 3D platformer game", genres.FirstOrDefault(g => g.Name == "Platform")?.Id ?? string.Empty, 120.99m, new DateOnly(2017, 10, 27)),
-   Game.Create("Red Dead Redemption 2", "An epic Western action-adventure game", genres.FirstOrDefault(g => g.Name == "Action-adventure")?.Id ?? string.Empty, 150.99m, new DateOnly(2018, 10, 26))
-];
+//fake database
+GameStoreData gameStoreData = new();
 
 app.MapGet("/", () => "Welcome to the Game Store API!");
 
 //GEt /genres
 app.MapGet("/genres", () => Results.Ok(
-  genres.Select(g =>
+  gameStoreData.GetGenres().Select(g =>
   new GenreDto(
     g.Id,
     g.Name))));
 
 // GET /games
 app.MapGet("/games", () => Results.Ok(
-    games.Select(g =>
+    gameStoreData.GetGames().Select(g =>
         new GameSummaryDto(
           g.Id,
           g.Name,
@@ -45,68 +33,68 @@ app.MapGet("/games", () => Results.Ok(
 // GET /games/{id}
 app.MapGet("/games/{id}", (string id) =>
 {
-  Game? game = games.FirstOrDefault(g => g.Id == id);
+    Game? game = gameStoreData.GetGame(id);
 
-  GameDetailDto result = GameDetailDto.Create(
-    game?.Id ?? string.Empty,
-    game?.Name ?? string.Empty,
-    game?.Description ?? string.Empty,
-    game?.Price ?? 0m,
-    game?.ReleaseDate ?? DateOnly.MinValue,
-    game?.GenreId ?? string.Empty
-  );
+    GameDetailDto result = GameDetailDto.Create(
+      game?.Id ?? string.Empty,
+      game?.Name ?? string.Empty,
+      game?.Description ?? string.Empty,
+      game?.Price ?? 0m,
+      game?.ReleaseDate ?? DateOnly.MinValue,
+      game?.GenreId ?? string.Empty
+    );
 
-  return game is not null ? Results.Ok(result) : Results.NotFound();
+    return game is not null ? Results.Ok(result) : Results.NotFound();
 })
 .WithName(GetGameEndpointName);
 
 // POST /games
 app.MapPost("/games", (CreateGame game) =>
 {
-  Genre? genre = genres.FirstOrDefault(g => g.Id == game.GenreId);
-  if (genre is null)
-  {
-    return Results.BadRequest($"Genre with ID '{game.GenreId}' does not exist.");
-  }
+    Genre? genre = gameStoreData.GetGenres().FirstOrDefault(g => g.Id == game.GenreId);
+    if (genre is null)
+    {
+        return Results.BadRequest($"Genre with ID '{game.GenreId}' does not exist.");
+    }
 
-  Game newGame = Game.Create(game.Name, game.Description, game.GenreId, game.Price, game.ReleaseDate);
-  games.Add(newGame);
+    Game newGame = Game.Create(game.Name, game.Description, game.GenreId, game.Price, game.ReleaseDate);
+    gameStoreData.AddGame(newGame);
 
-  return Results.CreatedAtRoute(
-    GetGameEndpointName,
-    new { id = newGame.Id },
-    newGame);
+    return Results.CreatedAtRoute(
+      GetGameEndpointName,
+      new { id = newGame.Id },
+      newGame);
 });
 
 //PUT /games/{id}
 app.MapPut("/games/{id}", (string id, Game updatedGame) =>
 {
-  Game? existingGame = games.FirstOrDefault(g => g.Id == id);
+    Game? existingGame = gameStoreData.GetGame(id);
 
-  if (existingGame is null)
-    return Results.NotFound();
+    if (existingGame is null)
+        return Results.NotFound();
 
-  existingGame.Name = updatedGame.Name;
-  existingGame.Description = updatedGame.Description;
-  existingGame.GenreId = updatedGame.GenreId;
-  existingGame.Price = updatedGame.Price;
-  existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    existingGame.Name = updatedGame.Name;
+    existingGame.Description = updatedGame.Description;
+    existingGame.GenreId = updatedGame.GenreId;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
 
-  return Results.NoContent();
+    return Results.NoContent();
 })
 .WithParameterValidation();
 
 //DELETE /games/{id}
 app.MapDelete("/games/{id}", (string id) =>
 {
-  Game? game = games.FirstOrDefault(g => g.Id == id);
-  if (game is null)
-  {
-    return Results.NotFound();
-  }
+    Game? game = gameStoreData.GetGame(id);
+    if (game is null)
+    {
+        return Results.NotFound();
+    }
 
-  games.Remove(game);
-  return Results.NoContent();
+    gameStoreData.RemoveGame(game);
+    return Results.NoContent();
 })
 .WithParameterValidation();
 
